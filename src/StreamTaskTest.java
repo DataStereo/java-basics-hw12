c3a5f13d563d974b7d22cc4251c724e8a0c3ece7
Oleh Morozov <morozov89oleg@gmail.com>
2023-09-24 18:27:12 +0300

    Add test data and implemented methods

--- src/StreamTaskTest.java
+++ src/StreamTaskTest.java
@@ -19,8 +19,15 @@ public class StreamTaskTest {
                 {
                         new int[]{13, -2, 33, -4, 5},
                         20
+                },
+                {
+                        new int[]{}, // Test with an empty array
+                        0
+                },
+                {
+                        new int[]{2, 4, 6, 8}, // Test with all even numbers
+                        120
                 }
-                // TODO add 2 more test data here
         };
     }
 
@@ -38,8 +45,15 @@ public class StreamTaskTest {
                 {
                         new int[]{130, -200, 330, -40, 50, 66},
                         new int[]{}
+                },
+                {
+                        new int[]{}, // Test with an empty array
+                        new int[]{}
+                },
+                {
+                        new int[]{7, 3, 1, 9, 5}, // Test with all odd numbers
+                        new int[]{1, 3, 5, 7, 9}
                 }
-                // TODO add 2 more test data here
         };
     }
 
